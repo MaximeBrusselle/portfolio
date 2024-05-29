511a19eddf3bc8d383f2e3ccce0772b76f892d8c
Maxime Brusselle <brussma@cronos.be>
2024-05-30 01:41:40 +0200

    add loading image to homepage

--- src/components/home/Photo.tsx
+++ src/components/home/Photo.tsx
@@ -1,13 +1,31 @@
 import PFP from "@/assets/PFP.png";
+import { Show, createSignal } from "solid-js";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function Photo() {
+    const [loading, setLoading] = createSignal(true);
+
+    const handleImageLoad = () => {
+        setLoading(false);
+    };
+
+    const handleImageError = () => {
+        setLoading(false);
+    };
+
     return (
-        <div class="relative h-full w-full avatar">
-            <div class="aspect-square w-[298px] xl:w-[498px] rounded-full ring ring-accent ring-offset-primary ring-offset-8">
+        <div class="avatar relative h-full w-full">
+            <div class="aspect-square w-[298px] rounded-full ring ring-accent ring-offset-8 ring-offset-primary xl:w-[498px]">
+                <Show when={loading()}>
+                    <Skeleton class="aspect-square w-[298px] shrink-0 rounded-full bg-gray-500/20 xl:w-[498px]" />
+                </Show>
                 <img
                     src={PFP}
                     alt="Picture of myself"
-                    class="rounded-full w-full h-full object-cover"
+                    onload={handleImageLoad}
+                    onerror={handleImageError}
+                    class="h-full w-full rounded-full object-cover"
+                    classList={{ hidden: loading() }}
                 />
             </div>
         </div>
